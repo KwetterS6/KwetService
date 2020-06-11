ed9f9bd53a6d148ef86b2128a4f65fa8663d7ad8
Wouter Vandenboorn <wouter04316@gmail.com>
2020-06-11 18:49:37 +0200

    addcors

--- KwetService/Startup.cs
+++ KwetService/Startup.cs
@@ -28,6 +28,9 @@ namespace KwetService
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddCors();
+            
             services.AddControllers();
 
             services.Configure<KwetstoreDatabaseSettings>(
@@ -52,7 +55,7 @@ namespace KwetService
             app.UseRouting();
             
             app.UseCors(x => x
-                .AllowAnyOrigin()
+                . AllowAnyOrigin()
                 .AllowAnyHeader()
                 .AllowAnyMethod()
             );
